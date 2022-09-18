92b368c8a4b46d767e530fe588842f06d8e9f2cc
= <=>
2022-09-18 22:08:14 +0530

    updated lint

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import createEmotionServer from '@emotion/server/create-instance';
 import Document, {
-    Head, Html, Main, NextScript
+    Head, Html, Main, NextScript,
 } from 'next/document';
 import * as React from 'react';
 import createEmotionCache from '../utility/createEmotionCache';
